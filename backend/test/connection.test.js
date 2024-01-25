21fc98bdb07d5029e7c2b4138df308084bbbd471
Eduardo Bravo <yahir.eduardo.bravo@gmail.com>
2024-01-25 17:20:03 -0500

    Feat(backend-test-connection)

--- backend/test/connection.test.js
+++ backend/test/connection.test.js
@@ -1,26 +1,50 @@
 import 'dotenv/config'
-import { expect, test, beforeAll, afterAll } from 'vitest'
+import { expect, test, suite, beforeAll, afterAll } from 'vitest'
 import { destroyConnection, getConnection, initDb } from '../src/connection'
 import fs from 'fs'
 
 const dbFile = process.env.DATABASE_FILE
 const testDbFile = 'data/db/test.sqlite'
 
+/**
+ * Removes the specified database file if it exists.
+ *
+ * @param {string} dbFile - the path of the database file to be removed
+ * @return {void}
+ */
+const removeDbFile = (dbFile) => {
+  if (fs.existsSync(dbFile)) {
+    fs.unlinkSync(dbFile)
+  }
+}
+// setup
 beforeAll(async () => {
   process.env.DATABASE_FILE = testDbFile
   destroyConnection()
-  if (fs.existsSync(process.env.DATABASE_FILE)) {
-    fs.unlinkSync(process.env.DATABASE_FILE)
-  }
+  removeDbFile(testDbFile)
   await initDb()
 })
 
+// teardrop
 afterAll(() => {
   destroyConnection()
   process.env.DATABASE_FILE = dbFile
+  removeDbFile(testDbFile)
 })
 
-test('check connection status', () => {
-  const knex = getConnection()
-  expect(knex).toBeTruthy()
+suite('Database connections and setup', () => {
+  test('check connection status', () => {
+    const knex = getConnection()
+    expect(knex).toBeTruthy()
+  })
+  test('check migrations status', async () => {
+    const knex = getConnection()
+    const status = await knex.migrate.status()
+    expect(status).toBe(0)
+  })
+  test('check migrations list', async () => {
+    const knex = getConnection()
+    const list = await knex.migrate.list()
+    expect(list).toBeTypeOf('object')
+  })
 })
