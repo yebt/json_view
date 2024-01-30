c891150781e5e6c01e89933fcaa5e173c8ffb095
Eduardo Bravo <yahir.eduardo.bravo@gmail.com>
2024-01-30 12:11:18 -0500

    Feat(backend): init the formatter, linter, and project struct

--- backend/src/modules/users/domain/User.js
+++ backend/src/modules/users/domain/User.js
@@ -1,22 +1,22 @@
 class User {
-    constructor(id, username, email, passwordHash) {
-      this.id = id;
-      this.username = username;
-      this.email = email;
-      this.passwordHash = passwordHash;
-    }
-  
-    // Additional methods or validations can be added here
-  
-    toJSON() {
-      // Convert the user object to a JSON representation for serialization
-      return {
-        id: this.id,
-        username: this.username,
-        email: this.email,
-        // Omit passwordHash in the JSON representation for security reasons
-      };
+  constructor (id, username, email, passwordHash) {
+    this.id = id
+    this.username = username
+    this.email = email
+    this.passwordHash = passwordHash
+  }
+
+  // Additional methods or validations can be added here
+
+  toJSON () {
+    // Convert the user object to a JSON representation for serialization
+    return {
+      id: this.id,
+      username: this.username,
+      email: this.email
+      // Omit passwordHash in the JSON representation for security reasons
     }
   }
-  
-  module.exports = User;
+}
+
+export default User
