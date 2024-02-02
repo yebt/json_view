c408604305ff1917d8ae5eab68c2bc9e44698ad7
Eduardo Bravo <yahir.eduardo.bravo@gmail.com>
2024-02-02 12:15:31 -0500

    feat(backend): init the server file

---
+++ backend/src/server.js
@@ -0,0 +1,13 @@
+import 'dotenv/config'
+// import { Express } from 'express'
+
+// const PORT = process.env.PORT || 3000
+
+// const app = new Express()
+
+// app.listen(PORT, () => {
+//   console.log(`Listening on port ${PORT}`)
+//   if (process.env.NODE_ENV !== 'test') {
+//     console.log(`http://localhost:${PORT}`)
+//   }
+// })
