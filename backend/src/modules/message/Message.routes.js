6caea80a346767e784641a5873a1d784e3359846
Eduardo Bravo <yahir.eduardo.bravo@gmail.com>
2024-02-05 12:15:57 -0500

    feat(backend): add support to save all in routes and call the formatted index, fix the order to delete all

--- backend/src/modules/message/Message.routes.js
+++ backend/src/modules/message/Message.routes.js
@@ -1,21 +1,44 @@
 import { Router } from 'express'
 import MessageHTTPAdapter from './Message.responses.js'
+import fs from 'fs'
 
 const messageRouter = Router()
+
+/**
+ * Saves all request information to a log file.
+ *
+ * @param {import('http').IncomingMessage} req - the request object
+ * @param {import('http').ServerResponse} res - the response object
+ * @param {Function} next - the next function to be called
+ * @return {void}
+ */
+const saveAllRequestInfo = (req, res, next) => {
+  // console.log(req.headers, req.headersDistinct)
+  if (!fs.existsSync('logs/')) {
+    fs.mkdirSync('logs/')
+  }
+  fs.writeFileSync(
+    `logs/request.${new Date().getTime()}.log`,
+    JSON.stringify([req.headers, req.headersDistinct, req.method, req.url, req.query, req.body], null, 2)
+  )
+  next()
+}
 
 // Save a new message
 messageRouter.post('/', MessageHTTPAdapter.storeMessageCommand)
 // save a new debug message
-messageRouter.post('/debug', MessageHTTPAdapter.storeDebuggMessageCommand)
+messageRouter.post('/debug', saveAllRequestInfo, MessageHTTPAdapter.storeDebuggMessageCommand)
 // Paginate messages
 messageRouter.get('/', MessageHTTPAdapter.listMessagesCommand)
+// Paginate Formatted messages
+messageRouter.get('/formatted', MessageHTTPAdapter.listFormatedMessagesCommand)
 // Get a specific message
 messageRouter.get('/:UUID', MessageHTTPAdapter.showMessageCommand)
 // Update a message
 messageRouter.put('/:UUID', MessageHTTPAdapter.patchMessageCommand)
-// Delete a message
-messageRouter.delete('/:UUID', MessageHTTPAdapter.deleteMessageCommand)
 // Clear all messages
 messageRouter.delete('/all', MessageHTTPAdapter.deleteAllMessageCommand)
+// Delete a message
+messageRouter.delete('/:UUID', MessageHTTPAdapter.deleteMessageCommand)
 
 export default messageRouter
